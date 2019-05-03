94ab9ad423b4d55a44d5362a2ff71cbb8f871a2f
22Santka <49483170+22santka@users.noreply.github.com>
2019-05-03 08:07:06 -0400

    Add files via upload

---
+++ modb_distance.py
@@ -0,0 +1,5 @@
+print("\nThis program converts kilometers(km) into miles(mi)\n")
+kilom = float(input("How many kilometers do you want to convert into miles? "))
+m = 0.62
+x = kilom * 0.62
+print(x,"miles")
---
+++ modb_unitconversion.py
@@ -0,0 +1,5 @@
+print("\nThis program converts yards(yd)) into inches(in)")
+kilom = float(input("How many yards do you want to convert into inches? "))
+m = 0.62
+x = kilom * 36
+print(x,"inches.")
